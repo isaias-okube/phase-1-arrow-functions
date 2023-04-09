72b13f81d98d0ce3f3ac90f5010699d8a2b92b73
ISAIAS <issuyep@yahoo.com>
2023-04-08 22:38:50 -0600

    complete lab

---
+++ index.js
@@ -0,0 +1,15 @@
+function divide() {
+    return 'bar';
+  }
+  let a = 2000;
+  let b = 100;
+function divide(a, b) {
+    const divide = (2000 / 100)
+    return 20;
+}
+let x = 2;
+const square=(x,) => (x)**2; 
+
+let parameter1 = 3
+let parameter2 = 4
+const add = (parameter1, parameter2) => parameter1 + parameter2;
